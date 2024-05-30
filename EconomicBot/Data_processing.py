aa2702b80f666bcac416e0bf77ba225f88345a83
-Cosgerms <135884105+Cofatyoi@users.noreply.github.com>
2024-05-30 11:47:04 +0300

    Update Data_processing.py
    LvlDataUpdating

--- EconomicBot/Data_processing.py
+++ EconomicBot/Data_processing.py
@@ -19,6 +19,10 @@ class SQLProcessing():
             member_data = cursor.fetchone()
             if member_data is None:
                 cursor.execute("INSERT INTO users(name, id, cash, rep, lvl, ecclass, job, inventory, ban, mute, kick) VALUES (?, ?, 0, 0, 1, ?, ?, ?, ?, ?, ?)", (str(member), member.id, "Отсутствует", "Подработка", "Ничего", "Нету", "Нету", "Нету"))
+        def CheckLvlMember(self, rep):
+            cursor = self.conn.cursor()
+            LvlDataLoading = cursor.execute("SELECT rep, lvl FROM users WHERE id = ?", (member.id,))
+            
 
 class CooldownProcessing():
     @commands.Cog.listener()
